92211168852d38695fc1e41d6a6e8596171c4628
Alejandro Chazarreta <lcrender@gmail.com>
2023-02-10 15:04:46 +0100

    add try_catch

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -5,6 +5,7 @@ const authCtrl = {};
 const Answers = require('../classes/Answers');
 
 authCtrl.signUp = async (req, res, next) => {
+try {
 	const admin = await User.find({});
 	if (admin.length === 1) {
 		const answer = await new Answers((message = 'There is already one admin. Please Login.'));
@@ -26,24 +27,31 @@ authCtrl.signUp = async (req, res, next) => {
 		});
 		const answer = await new Answers((message = { auth: true, token }));
 		return res.status(201).json(answer);
-	}
+		}
+	} catch (error) {
+		res.status(500).json({message: error});
+	} 
 };
 authCtrl.logIn = async (req, res, next) => {
-	const { username, password } = req.body;
-	const user = await User.findOne({ username: username });
-	if (!user) {
-		const answer = await new Answers((message = 'The Username doesn´t exist'));
-		return res.status(401).json(answer);
+	try {
+		const { username, password } = req.body;
+		const user = await User.findOne({ username: username });
+		if (!user) {
+			const answer = await new Answers((message = 'The Username doesn´t exist'));
+			return res.status(401).json(answer);
+		}
+		const validPassword = await user.validatePassword(password);
+		if (!validPassword) {
+			const answer = await new Answers((message = { auth: false, token: null }));
+			return res.status(401).json(answer);
+		}
+		const token = jwt.sign({ id: user._id }, JWTSECRET, {
+			expiresIn: 60 * 60 * 1
+		});
+		const answer = await new Answers((message = { auth: true, token }));
+		return res.status(201).json(answer);
+	} catch (error) {
+		res.status(500).json({message: error});
 	}
-	const validPassword = await user.validatePassword(password);
-	if (!validPassword) {
-		const answer = await new Answers((message = { auth: false, token: null }));
-		return res.status(401).json(answer);
-	}
-	const token = jwt.sign({ id: user._id }, JWTSECRET, {
-		expiresIn: 60 * 60 * 1
-	});
-	const answer = await new Answers((message = { auth: true, token }));
-	return res.status(201).json(answer);
 };
 module.exports = authCtrl;
